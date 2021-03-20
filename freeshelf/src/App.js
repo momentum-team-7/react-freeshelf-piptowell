949156da55e7ff93b46d24d5fa8ad10a3505df16
Tip Powell <powelltip@gmail.com>
2021-03-19 23:40:11 -0400

    how we come in / how we come out / its rendering lets goooooo

--- freeshelf/src/App.js
+++ freeshelf/src/App.js
@@ -1,29 +1,38 @@
-import React, { useState } from 'react'
+import React from 'react'
 import bookdata from './bookdata'
 
 
 
 function App() {
-  const books = ["Moby Dick", "1984", "Farenheit 451"]
-  // const [books, setBooks] = useState()
+ // const [books, setBooks] = useState()
   return (
     <div>
       <h1>Book</ h1>
-      {books.map((book) => {
-        console.log({book})
-        return <Book title={book} />
+      {bookdata.map((book, idx) => {
+        return <Book title={book.title} 
+        author={book.author} 
+        shortDescription={book.shortDescription} 
+        coverImageUrl={book.coverImageUrl} 
+        publisher={book.publisher} 
+        publicationDate={book.publicationDate} 
+        detailedDescription={book.detailedDescription} 
+        key={idx}/>
       })}
     </div>
-  );
+  )
 }
 
 const Book = (props) => {
   const {title, author, shortDescription, coverImageUrl, publisher, publicationDate, detailedDescription } = props
   return (
   <div className='book'>
-  <h2>{title}</h2>
-  <p>testing :P</p>
-  <h3>{author}</h3>
+    <h2>{title}</h2>
+    <h3>{author}</h3>
+    <p>{shortDescription}</p>
+    <img src={coverImageUrl}></img>
+    <p>{publisher}</p>
+    <p>{publicationDate}</p>
+    <p>{detailedDescription}</p>
   </div>
   )
 }
--- freeshelf/src/bookdata.js
+++ freeshelf/src/bookdata.js
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const bookdata = [
   {
     "title": "You Don't Know JS Yet: Getting Started",
